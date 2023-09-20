0284ce2b79db4744bbdbb6b724fe4e061e6595cc
Sahin Nayak <89744970+Sahin-Nayak@users.noreply.github.com>
2023-09-20 09:29:05 +0530

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,50 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Konkal",
+    artist: "Sahin,Srabanti,Parthib,Rohan",
+    img: "music-1",
+    src: "KONKAL FINAL"
+  },
+  {
+    name: "Kheya",
+    artist: "Sahin,Shinjini,Avishikta,",
+    img: "music-2",
+    src: "atfinal wav_02"
+  },
+  {
+    name: "o nodi kano firiye dile",
+    artist: "Shinjini",
+    img: "music-3",
+    src: "o nodi kano firiye dile_01_01"
+  },
+  {
+    name: "obosese",
+    artist: "Shinjini",
+    img: "music-4",
+    src: "obosese_for_edit56_01"
+  },
+  {
+    name: "Somadhi",
+    artist: "Shinjini",
+    img: "music-5",
+    src: "Snapinsta.app_video_664B7C0D32CCBE7D85A49DA3BAA82FA7_video_dashinit"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
